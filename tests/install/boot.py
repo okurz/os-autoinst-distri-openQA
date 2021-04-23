2b06693baec70a62701c2a465ca6ad8296036db6
Oliver Kurz <okurz@suse.de>
2021-04-23 06:33:14 +0000

    wip

--- tests/install/boot.py
+++ tests/install/boot.py
@@ -1,7 +1,7 @@
 from testapi import *
 
 def run(self):
-    assert_screen('boot-menu', 'openqa-desktop'])
+    assert_screen(['boot-menu', 'openqa-desktop'])
     if match_has_tag('boot-menu'):
         send_key('ret')
     assert_screen('openqa-desktop', 500)
