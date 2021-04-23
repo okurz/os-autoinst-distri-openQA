775b6e24afbb774652957e8b17fe708878fa15ad
Oliver Kurz <okurz@suse.de>
2021-04-23 08:41:28 +0000

    wip

--- tests/install/boot.py
+++ tests/install/boot.py
@@ -4,6 +4,6 @@ def run(self):
     assert_screen(['boot-menu', 'openqa-desktop'])
     if match_has_tag('boot-menu'):
         send_key('ret')
-    assert_and_click('openqa-desktop-login', 500)
+    assert_and_click('openqa-desktop-login', timeout=500)
     select_console('root-console')
     assert_script_run('systemctl start openqa-webui')
